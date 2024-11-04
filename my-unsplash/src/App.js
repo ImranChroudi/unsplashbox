94902cc7188ba41f27590a60056089e6c3b4d115
ImranChroudi <chroudiimran@gmail.com>
2024-11-04 18:16:27 +0100

    added header and add ressources and add botstrap

--- my-unsplash/src/App.js
+++ my-unsplash/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
 
 const App = () => {
   return (
-    <div>
-      <h1>My Unsplash</h1>
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<SearchPage />} />
+       {/* <Route path="/about" element={<About />} />
+       <Route path="/contact" element={<Contact />} />  */}
+     </Routes>
+  </Router>
   )
 }
 
